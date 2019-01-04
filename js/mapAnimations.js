ce94e017d3af32b4562c8f25b7afa454ecb4aae1
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2019-01-04 16:32:43 -0500

    added scroll tracking

--- js/mapAnimations.js
+++ js/mapAnimations.js
@@ -1,4 +1,31 @@
+var geojson = { 
+    "type": "FeatureCollection",
+    "name": "Appalachian Trail",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    ]
+};
+
+//Scroll percentage function taken (with much gratitude) from https://stackoverflow.com/questions/2387136/cross-browser-method-to-determine-vertical-scroll-percentage-in-javascript/9348993
+$(window).on('scroll', function(){
+    var s = $(window).scrollTop(),
+        d = $(document).height(),
+        c = $(window).height();
+  
+    var scrollPercent = (s / (d - c)) * 100;
+    
+    var at_progress = (scrollPercent/86.82767564293172) * 100;
+    if (at_progress > 100){
+        at_progress = 100;
+    }
+    console.clear();
+    console.log(at_progress);
+  })
+
+
+
 document.addEventListener("DOMContentLoaded", function(event){
+    console.log(at_centerline["features"][0])
     mapboxgl.accessToken = 'pk.eyJ1IjoiYWFyb25icmV6ZWwiLCJhIjoiY2pwNXNyb3IxMDJwZTNxbzZ4M3IxdGp5ZCJ9.1_DVjqU_cgiK9gt-LGf3DA';
     const map = new mapboxgl.Map({
     container: 'map',
